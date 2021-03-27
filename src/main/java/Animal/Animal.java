5e7c2ad2ee9bea8d1bb094695582a2749dbdca4f
Dawar Aamir <dawar.aamir@gmail.com>
2021-03-27 13:40:40 -0400

    Dawars commit

--- src/main/java/Animal/Animal.java
+++ src/main/java/Animal/Animal.java
@@ -3,4 +3,7 @@ package Animal;
 import Interfaces.*;
 
 public class Animal implements Eater, NoiseMaker {
+    public static void main(String[] args) {
+        System.out.println("This is the animal class");
+    }
 }
