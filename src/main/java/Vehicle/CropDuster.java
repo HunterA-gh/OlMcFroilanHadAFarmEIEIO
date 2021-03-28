6a4ace97281afd63dde77bb62bd01e627ef4a9e6
dawaraamir <80354294+dawaraamir@users.noreply.github.com>
2021-03-27 20:42:51 -0400

    Update CropDuster.java

--- src/main/java/Vehicle/CropDuster.java
+++ src/main/java/Vehicle/CropDuster.java
@@ -4,5 +4,25 @@ import Interfaces.FarmVehicle;
 
 public class CropDuster extends Aircraft implements FarmVehicle {
 
+public void fly() {
 
+    }
+
+    public void operate(Farm farm) {
+
+    }
+
+    public String makeNoise() {
+
+        return "Plane engine";
+    }
+
+    public void mount() {
+
+    }
+
+    public void dismount() {
+
+    }
+  
 }
