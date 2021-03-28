11fe5f03370c3773d968ac2f9f2e6de76d754565
dawaraamir <80354294+dawaraamir@users.noreply.github.com>
2021-03-27 20:42:05 -0400

    Update Tractor.java

--- src/main/java/Vehicle/Tractor.java
+++ src/main/java/Vehicle/Tractor.java
@@ -4,5 +4,25 @@ import Interfaces.FarmVehicle;
 
 public class Tractor extends Vehicle implements FarmVehicle {
 
+  public Crop harvest(Crop crop) {
+        return new crop();
+    }
+
+    public void operate(Farm farm) {
+
+    }
+
+    public String makeNoise() {
+
+        return "Tractor noises";
+    }
+
+    public void mount() {
+
+    }
+
+    public void dismount() {
+
+    }
 
 }
