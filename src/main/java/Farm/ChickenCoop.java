926255c411f93a13e7678c896a0c830bf76722e6
Jonathin <jonathinmakori@gmail.com>
2021-03-27 23:56:48 -0400

    Added Farm

--- src/main/java/Farm/ChickenCoop.java
+++ src/main/java/Farm/ChickenCoop.java
@@ -1,4 +1,14 @@
 package Farm;
 
+import Animals.Chicken;
+
+import java.util.ArrayList;
+
 public class ChickenCoop {
+
+    public ArrayList<Chicken> Chickens = new ArrayList<>();
+
+    public void addChicken(Chicken chicken) {
+        this.Chickens.add(chicken);
+    }
 }
