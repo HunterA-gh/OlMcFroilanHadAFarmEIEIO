21b4d7259eb8446ad9aeaf9df6ea4c4e09e3174f
jonyboi21 <75004568+jonyboi21@users.noreply.github.com>
2021-03-28 12:38:22 -0400

    Update FarmHouse.java

--- src/main/java/Farm/FarmHouse.java
+++ src/main/java/Farm/FarmHouse.java
@@ -1,16 +1,31 @@
 package Farm;
 
 import Person.Person;
+import Vehicle.CropDuster;
 
 import java.util.ArrayList;
 
 public class FarmHouse {
 
+    public FarmHouse() {
+    }
+
+    public FarmHouse(ArrayList<Person> farmPeople) {
+        this.farmPeople = farmPeople;
+    }
+
     public ArrayList<Person> farmPeople = new ArrayList<Person>();
+
+
 
     public void addPerson(Person person){
         this.farmPeople.add(person);
-
+    }
+    public void removePerson(Person person){
+        this.farmPeople.remove(person);
+    }
+    public ArrayList getFarmPeople(){
+        return farmPeople;
     }
 
 }
