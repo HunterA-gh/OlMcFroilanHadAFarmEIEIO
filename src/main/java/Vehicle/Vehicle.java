081ee6a199a96e6c4fdf4b367a5f1ce34328949c
dawaraamir <80354294+dawaraamir@users.noreply.github.com>
2021-03-27 20:41:22 -0400

    Update Vehicle.java

--- src/main/java/Vehicle/Vehicle.java
+++ src/main/java/Vehicle/Vehicle.java
@@ -3,5 +3,13 @@ package Vehicle;
 import Interfaces.*;
 
 public abstract class Vehicle implements Rideable, NoiseMaker {
+  
+  public void startRiding() {
+
+    }
+
+    public void stopRiding() {
+
+    }
 
 }
